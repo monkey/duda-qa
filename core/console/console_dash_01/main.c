b417f78584955254b1d63a92ebfabb5b91a2051d
Eduardo Silva <eduardo@monkey.io>
2014-09-24 16:49:14 -0600

    core/console/console_dash_01: new unit test
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

---
+++ core/console/console_dash_01/main.c
@@ -0,0 +1,12 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include "webservice.h"
+
+DUDA_REGISTER("Duda QA", "Unit Test");
+
+int duda_main()
+{
+    /* test dashboard redirect */
+    console->dashboard("/dash");
+    return 0;
+}
